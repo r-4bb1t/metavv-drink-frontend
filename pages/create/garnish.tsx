99288f5c56038401097ffbed905804ffced50286
유영인 <asst2207051@tosspartners.com>
2022-10-14 21:55:12 +0900

    feat: 재료 선택 페이지 추가

---
+++ pages/create/garnish.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import { CreateHeader } from "../../components/CreateHeader";
+import { IngredientButton } from "../../components/IngredientButton";
+import { Layout } from "../../components/Layout";
+
+const Garnish: NextPage = () => {
+  return (
+    <Layout hasHeader>
+      <CreateHeader title="1. 기주" subTitle="2팀과 나의 우정 도수는?" />
+      <IngredientButton.Big
+        label="보드카 (90º)"
+        description="우린 영혼의 단짝 친구지!"
+        checked={true}
+      />
+    </Layout>
+  );
+};
+
+export default Garnish;
