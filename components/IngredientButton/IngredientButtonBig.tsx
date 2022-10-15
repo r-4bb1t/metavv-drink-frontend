0f5f388f32f2209bdb8e23c4d6667be02a801126
유영인 <asst2207051@tosspartners.com>
2022-10-15 14:03:56 +0900

    feat: ingredient button에 checked, onChange prop 추가

--- components/IngredientButton/IngredientButtonBig.tsx
+++ components/IngredientButton/IngredientButtonBig.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import styles from "../../styles/IngredientButton.module.scss";
 
 interface Props {
@@ -5,7 +6,12 @@ interface Props {
   label: string;
   description: string;
   value: string;
-  onClick: () => void;
+  onClick?: () => void;
+  checked?: boolean;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => void;
 }
 
 export function IngredientButtonBig({
@@ -14,7 +20,18 @@ export function IngredientButtonBig({
   description,
   value,
   onClick,
+  checked,
+  onChange,
 }: Props) {
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (onChange !== undefined) {
+        onChange(event, event.target.checked);
+      }
+    },
+    [onChange]
+  );
+
   return (
     <div className={styles.container_big}>
       <input
@@ -24,6 +41,8 @@ export function IngredientButtonBig({
         value={value}
         id={id}
         onClick={onClick}
+        checked={checked}
+        onChange={handleInputChange}
       />
       <label htmlFor={id}>
         <span className={styles.main}>{label}</span>
--- components/IngredientButton/IngredientButtonSmall.tsx
+++ components/IngredientButton/IngredientButtonSmall.tsx
@@ -1,20 +1,48 @@
+import { useCallback } from "react";
 import styles from "../../styles/IngredientButton.module.scss";
 
 interface Props {
+  id: string;
   label: string;
-  checked: boolean;
+  value: string;
+  onClick?: () => void;
+  checked?: boolean;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => void;
 }
 
-export function IngredientButtonSmall({ label, checked }: Props) {
+export function IngredientButtonSmall({
+  id,
+  label,
+  value,
+  onClick,
+  checked,
+  onChange,
+}: Props) {
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (onChange !== undefined) {
+        onChange(event, event.target.checked);
+      }
+    },
+    [onChange]
+  );
+
   return (
     <div className={styles.container_small}>
       <input
         className={styles["visually-hidden"]}
         type="checkbox"
-        id="ingredient-button-small"
+        name="ingredient"
+        value={value}
+        id={id}
+        onClick={onClick}
         checked={checked}
+        onChange={handleInputChange}
       />
-      <label htmlFor="ingredient-button-small">{label}</label>
+      <label htmlFor={id}>{label}</label>
     </div>
   );
 }
