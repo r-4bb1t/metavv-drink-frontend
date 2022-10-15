d6c43bc790b963cd41c11bca22c3d8d398d48dd8
유영인 <asst2207051@tosspartners.com>
2022-10-15 15:45:11 +0900

    feat: 주재료, 부재료 선택 페이지 뷰

--- pages/create/main.tsx
+++ pages/create/main.tsx
@@ -1,17 +1,62 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import { useState } from "react";
 import { CreateHeader } from "../../components/CreateHeader";
 import { IngredientButton } from "../../components/IngredientButton";
+import { ingredients } from "../../components/IngredientButton/constants";
 import { Layout } from "../../components/Layout";
+import common from "../../styles/Common.module.scss";
 
 const Main: NextPage = () => {
+  const [selectedMain, setSelectedMain] = useState<string[]>([]);
+
+  const ingredientButtons = ingredients.map((ingredient) => {
+    return (
+      <IngredientButton.Small
+        key={ingredient}
+        id={ingredient}
+        label={ingredient}
+        value={ingredient}
+        onChange={(_, checked: boolean) => {
+          if (checked) {
+            setSelectedMain([...selectedMain, ingredient]);
+          } else {
+            setSelectedMain(
+              selectedMain.filter((value) => value !== ingredient)
+            );
+          }
+        }}
+      />
+    );
+  });
+
   return (
     <Layout hasHeader>
-      <CreateHeader title="1. 기주" subTitle="2팀과 나의 우정 도수는?" />
-      <IngredientButton.Big
-        label="보드카 (90º)"
-        description="우린 영혼의 단짝 친구지!"
-        checked={true}
+      <CreateHeader
+        title="2. 주재료"
+        subTitle="2팀의 첫인상은 어땠어? (3개 선택)"
+        style={{ marginBottom: 15 }}
       />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {ingredientButtons}
+      </div>
+      <Link href="/create/sub">
+        <a>
+          <button
+            className={common.button}
+            disabled={selectedMain.length === 0}
+          >
+            다음
+          </button>
+        </a>
+      </Link>
     </Layout>
   );
 };
--- pages/create/sub.tsx
+++ pages/create/sub.tsx
@@ -1,17 +1,57 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import { useState } from "react";
 import { CreateHeader } from "../../components/CreateHeader";
 import { IngredientButton } from "../../components/IngredientButton";
+import { ingredients } from "../../components/IngredientButton/constants";
 import { Layout } from "../../components/Layout";
+import common from "../../styles/Common.module.scss";
 
 const Sub: NextPage = () => {
+  const [selectedSub, setSelectedSub] = useState<string[]>([]);
+
+  const ingredientButtons = ingredients.map((ingredient) => {
+    return (
+      <IngredientButton.Small
+        key={ingredient}
+        id={ingredient}
+        label={ingredient}
+        value={ingredient}
+        onChange={(_, checked: boolean) => {
+          if (checked) {
+            setSelectedSub([...selectedSub, ingredient]);
+          } else {
+            setSelectedSub(selectedSub.filter((value) => value !== ingredient));
+          }
+        }}
+      />
+    );
+  });
+
   return (
     <Layout hasHeader>
-      <CreateHeader title="1. 기주" subTitle="2팀과 나의 우정 도수는?" />
-      <IngredientButton.Big
-        label="보드카 (90º)"
-        description="우린 영혼의 단짝 친구지!"
-        checked={true}
+      <CreateHeader
+        title="3. 부재료"
+        subTitle="2팀의 현인상은 어때? (3개 선택)"
+        style={{ marginBottom: 15 }}
       />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {ingredientButtons}
+      </div>
+      <Link href="/create/garnish">
+        <a>
+          <button className={common.button} disabled={selectedSub.length === 0}>
+            다음
+          </button>
+        </a>
+      </Link>
     </Layout>
   );
 };
