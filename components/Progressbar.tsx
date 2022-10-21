ba2dc1b884c60e1a16c9f2d6c31b7580832ee62d
유영인 <asst2207051@tosspartners.com>
2022-10-21 15:39:31 +0900

    chore: progressbar 포매팅 및 변수명 조정

--- components/Progressbar.tsx
+++ components/Progressbar.tsx
@@ -2,25 +2,24 @@ import { useRouter } from "next/router";
 import { ReactNode, useState } from "react";
 import styles from "../styles/ProgressBar.module.scss";
 
-export const ProgressBar = ({page}:{page:String}) => {
-
-    const progressBar = [
-        {name: '재료 선택',
-        page: 'material'},
-        {name: '술잔 선택',
-        page: 'glass'},
-        {name: '이름 짓기',
-        page: 'naming'}
-    ]
-    return(
-        <div className={styles.progressBar}>
-        
-        {progressBar.map((a, i)=>(
-            <>
-                <span className={page === a.page ? styles.selected : styles.progressBar}>{a.name}</span>
-                <span > {i < 2 ? '>' : null} </span>
-            </>
-        ))}
-        </div>
-    )
-}
+export const ProgressBar = ({ page }: { page: String }) => {
+  const progressBar = [
+    { name: "재료 선택", page: "ingredient" },
+    { name: "술잔 선택", page: "glass" },
+    { name: "이름 짓기", page: "naming" },
+  ];
+  return (
+    <div className={styles.progressBar}>
+      {progressBar.map((a, i) => (
+        <>
+          <span
+            className={page === a.page ? styles.selected : styles.progressBar}
+          >
+            {a.name}
+          </span>
+          <span> {i < 2 ? ">" : null} </span>
+        </>
+      ))}
+    </div>
+  );
+};
