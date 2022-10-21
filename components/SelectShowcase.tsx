ccaee046ac9259a1e58eaca63b40f6b555e2a485
smreosms13 <smreosms13@gamil.com>
2022-10-21 21:20:14 +0900

    style/NEW 버튼 스타일 수정

--- components/SelectShowcase.tsx
+++ components/SelectShowcase.tsx
@@ -11,19 +11,10 @@ export const SelectShowcase = ({}: {}) => {
   return (
     <>
       {images.map((image) => (
-        <div className={styles.cardBox}>
-          <div className={styles.selected}>
-            <svg xmlns="http://www.w3.org/2000/svg">
-              <g mask="url(#mask0_174_177)">
-                <path
-                  d="M8.76079 16.2973L3.677 11.2135L4.94795 9.94254L8.76079 13.7554L16.9439 5.57227L18.2148 6.84321L8.76079 16.2973Z"
-                  fill="white"
-                />
-              </g>
-            </svg>
-          </div>
+        <button className={styles.cardBox} type={"button"}>
+          <div className={styles.selected}></div>
           <img className={styles.cardImg} src={image} alt="배경" />
-        </div>
+        </button>
       ))}
     </>
   );
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,15 +1,45 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import common from "../styles/Common.module.scss";
 import { Layout } from "../components/Layout";
-
-const Home: NextPage = () => {
+import { SelectBackground } from "../components/SelectBackground";
+import styles from "../styles/New.module.scss";
+import { SelectShowcase } from "../components/SelectShowcase";
+const New: NextPage = () => {
   return (
     <Layout hasHeader>
-      #크리에이터 화면 - 새 게임 만들기
-      <br />
-      /new
-      <br />
+      <div className={styles.nameContainer}>
+        <input
+          className={styles.nameBox}
+          type="text"
+          placeholder="이름 입력"
+          autoFocus
+          required
+        ></input>
+        <div className={styles.pencil}></div>
+        <div className={styles.showcaseHeader}>showcase</div>
+      </div>
+      <form action="POST" className={styles.form}>
+        <section>
+          <p>배경선택</p>
+          <div className={styles.container_slider}>
+            <SelectBackground></SelectBackground>
+          </div>
+        </section>
+        <section>
+          <p>쇼케이스 선택</p>
+          <div className={styles.container_slider}>
+            <SelectShowcase></SelectShowcase>
+          </div>
+        </section>
+        <Link href="/new">
+          <a>
+            <button className={styles.button}>제작 완료!</button>
+          </a>
+        </Link>
+      </form>
     </Layout>
   );
 };
 
-export default Home;
+export default New;
